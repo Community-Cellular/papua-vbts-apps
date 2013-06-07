d9d617d85475fc5aea8b7ccbd18733852aac4221
Kurtis Heimerl <kheimerl@cs.berkeley.edu>
2013-06-07 00:31:53 -0700

    munging

---
+++ village_idol/munge_votes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import re
+import numpy
+
+VOTE_RE = re.compile("(IMSI\d{15})\.gsm\.(IMSI\d{15})\.vote")
+
+VOTES = {}
+
+def insert_vote(target, vote):
+    if target not in VOTES:
+        VOTES[target] = []
+    VOTES[target].append(vote)
+
+for v in sys.argv[1:]:
+    m = VOTE_RE.match(v)
+    if not (m):
+        print ("Malformed vote: " + v)
+    else:
+        f = open(v)
+        vote = int(f.readline())
+        insert_vote(m.group(1), vote)
+
+print ("VOTE FILE,MEAN,MEDIAN")
+for t in VOTES.keys():
+    print (t + ".gsm," + str(numpy.mean(VOTES[t])) + "," + str(numpy.median(VOTES[t])))
